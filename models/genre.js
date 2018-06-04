d9410ec164a67fad166f14720391d23b2ef80255
siznax <steve@siznax.net>
2018-06-04 16:30:03 -0700

    Ran standard(.js) --fix on models

--- models/genre.js
+++ models/genre.js
@@ -1,7 +1,5 @@
-// siznax 26 May 2018
-
 var mongoose = require('mongoose')
 
 module.exports = mongoose.model('Genre', {
-    name: {type: String, min: 3, max: 100, required: true, unique: true}
+  name: {type: String, min: 3, max: 100, required: true, unique: true}
 })
--- models/work.js
+++ models/work.js
@@ -1,13 +1,12 @@
-// siznax 26 May 2018
-
 var mongoose = require('mongoose')
+var ObjectId = mongoose.Schema.Types.ObjectId
 
 module.exports = mongoose.model('Work', {
-    creator: [{type: String, required: true}],
-    genre: [{type: mongoose.Schema.Types.ObjectId, ref: 'Genre'}],
-    language: {type: String, default: 'English'},
-    mediatype: {type: String, enum: ['album', 'book', 'film'], required: true},
-    title: {type: String, required: true},
-    URL: String,
-    year: {type: String, required: true},
+  creator: [{type: String, required: true}],
+  genre: [{type: ObjectId, ref: 'Genre'}],
+  language: {type: String, default: 'English'},
+  mediatype: {type: String, enum: ['album', 'book', 'film'], required: true},
+  title: {type: String, required: true},
+  URL: String,
+  year: {type: String, required: true}
 })
