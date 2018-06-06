453e5fb8c012fbf9c5162b770a8055b555c3ef55
siznax <steve@siznax.net>
2018-06-06 13:24:41 -0700

    Renamed listController.js to goodsController.js

---
+++ controllers/goodsController.js
@@ -0,0 +1,56 @@
+// GOODS route handlers
+
+var Goods = require('../models/goods');
+var Work = require('../models/work');
+var Genre = require('../models/genre');
+
+var async = require('async');
+
+exports.index = function(req, res) {
+  async.parallel({
+    goodsCount: function (callback) { Goods.count({}, callback) },
+    workCount: function (callback) { Work.count({}, callback) },
+    genreCount: function (callback) { Genre.count({}, callback) }
+  },
+  function(err, results) {
+    res.render('index', {
+      title: 'Catalog',
+      error: err,
+      data: results
+    })
+  })
+}
+
+exports.goodsList = function (req, res, next) {
+  Goods.find({}, 'title year source')
+    .exec(function (err, goods) {
+      if (err) { return next(err) }
+      res.render('list_goods', {
+        title: 'List of Goods',
+        data: goods
+      })
+    })
+}
+
+exports.goodsDetail = function (req, res, next) {
+  res.send('goods detail')
+}
+
+exports.goodsCreateGet = function (req, res) {
+  res.send('goods create form')
+}
+exports.goodsCreatePost = function (req, res) {
+  res.send('goods create POST')
+}
+exports.goodsDeleteGet = function (req, res) {
+  res.send('goods delete form')
+}
+exports.goodsDeletePost = function (req, res) {
+  res.send('goods delete POST')
+}
+exports.goodsUpdateGet = function (req, res) {
+  res.send('goods update form')
+}
+exports.goodsUpdatePost = function (req, res) {
+  res.send('goods update POST')
+}
