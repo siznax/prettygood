efaad74b900d99831112f5bd4f21b0cb7097333b
siznax <steve@siznax.net>
2018-06-08 20:18:32 -0700

    Render goods works in order in goodsController

--- controllers/goodsController.js
+++ controllers/goodsController.js
@@ -1,18 +1,19 @@
 // GOODS route handlers
 
-var Goods = require('../models/goods');
-var Work = require('../models/work');
-var Genre = require('../models/genre');
+var Goods = require('../models/goods')
+var Work = require('../models/work')
+var Genre = require('../models/genre')
 
-var async = require('async');
+var async = require('async')
+var mongoose = require('mongoose')
 
-exports.index = function(req, res) {
+exports.index = function (req, res) {
   async.parallel({
     goodsCount: function (callback) { Goods.count({}, callback) },
     workCount: function (callback) { Work.count({}, callback) },
     genreCount: function (callback) { Genre.count({}, callback) }
   },
-  function(err, results) {
+  function (err, results) {
     res.render('index', {
       title: 'Catalog',
       error: err,
@@ -33,16 +34,48 @@ exports.goodsList = function (req, res, next) {
 }
 
 exports.goodsDetail = function (req, res, next) {
-  Goods.findById(req.params.id)
-    .populate('genre')
-    .exec(function (err, goods) {
-      if (err) { return next(err) }
-      res.render('detail_goods', {
-        title: 'Goods Detail',
-        goods: goods,
-        genres: goods.genre.map(x => x.name),
-        works: goods.works
-      })
+  async.waterfall([
+    function (callback) {
+      Goods.findById(req.params.id)
+        .populate('genre')
+        .exec(function (err, goods) {
+          if (err || !goods) {
+            var notfound = new Error()
+            err.status = 404
+            err.message = 'Could not find the goods: ' + req.params.id
+            return next(notfound)
+          }
+          callback(null, goods)
+        })
+    },
+    function (goods, callback) {
+      var ranks = JSON.parse(JSON.stringify(goods.works)) // FIXME
+      var ids = Array
+        .from(goods.works.values())
+        .map(x => mongoose.Types.ObjectId(x))
+      Work.find({_id: {$in: ids}}, '-genre -language')
+        .exec(function (err, works) {
+          if (err) { return next(err) }
+          callback(null, {'goods': goods, 'works': works, 'ranks': ranks})
+        })
+    }
+  ],
+  function (err, result) {
+    if (err) { return next(err) }
+
+    // map works by _id to display in ranked order
+    var worksMap = {}
+    result.works.forEach(function (work) {
+      worksMap[work._id] = work
+    })
+
+    res.render('detail_goods', {
+      title: 'Goods Detail',
+      goods: result.goods,
+      genres: result.goods.genre.map(x => x.name),
+      works: worksMap,
+      ranks: result.ranks
+    })
   })
 }
 
