97a7606ef715745eb686ad209a328522489c045c
siznax <steve@siznax.net>
2018-06-10 15:58:52 -0700

    Added description field to Work model.

--- models/work.js
+++ models/work.js
@@ -3,6 +3,7 @@ var ObjectId = mongoose.Schema.Types.ObjectId
 
 module.exports = mongoose.model('Work', {
   creator: [{type: String, required: true}],
+  description: String,
   genre: [{type: ObjectId, ref: 'Genre'}],
   language: {type: String, default: 'English'},
   mediatype: {type: String, enum: ['albums', 'books', 'film'], required: true},
