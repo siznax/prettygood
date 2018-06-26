c9fcd376804f19b79073d700c2053d03aea9151b
siznax <steve@siznax.net>
2018-06-26 09:33:09 -0700

    Added unique:true to select model members

--- models/goods.js
+++ models/goods.js
@@ -2,13 +2,13 @@ var mongoose = require('mongoose')
 var ObjectId = mongoose.Schema.Types.ObjectId
 
 module.exports = mongoose.model('Goods', {
-  description: {type: String},
+  description: {type: String, unique: true},
   genre: [{type: ObjectId, ref: 'Genre', required: true}],
-  image: String,
+  image: {type: String, unique: true},
   mediatype: {type: String},
   source: {type: String, required: true},
   title: {type: String, required: true},
-  url: {type: String},
+  url: {type: String, unique: true},
   works: {type: Map, of: ObjectId, ref: 'Work', required: true},
   year: {type: String, required: true}
 })
--- models/work.js
+++ models/work.js
@@ -3,11 +3,11 @@ var ObjectId = mongoose.Schema.Types.ObjectId
 
 module.exports = mongoose.model('Work', {
   creator: [{type: String, required: true}],
-  description: String,
+  description: {type: String, unique: true},
   genre: [{type: ObjectId, ref: 'Genre'}],
-  language: {type: String, default: 'English'},
+  language: {type: String, default: 'en'},
   mediatype: {type: String, enum: ['albums', 'books', 'films'], required: true},
   title: {type: String, required: true},
-  url: String,
+  url: {type: String, unique: true},
   year: {type: String, required: true}
 })
